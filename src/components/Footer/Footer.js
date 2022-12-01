2244abdf60c50fd39dc07e899e931174e798fa79
Cap0n3 <cap0n3@protonmail.com>
2022-12-01 12:03:34 +0100

    Started footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { FooterContainer } from "./Footer.style";
+import { 
+    FooterContainer, 
+    FooterWrapper, 
+    FtNavWrapper,
+    FtNavList,
+    FtNavItem,
+    SocialWrapper, 
+    LogoWrapper,
+    SiteLogo,
+    CopyrightWrapper
+} from "./Footer.style";
+
+import LogoWH from "../../assets/logos/VoiceUp_Logo_WH.png"
 
 const Footer = () => {
     return(
         <FooterContainer>
-            <p>Hello</p>
+            <FooterWrapper>
+                <FtNavWrapper>
+                    <FtNavList>
+                        <FtNavItem>F.A.Q</FtNavItem>
+                        <FtNavItem>Condition des cours</FtNavItem>
+                        <FtNavItem>Tarifs</FtNavItem>
+                        <FtNavItem>Localisation</FtNavItem>
+                    </FtNavList>
+                </FtNavWrapper>
+                <SocialWrapper></SocialWrapper>
+                <hr />
+                <LogoWrapper>
+                    <SiteLogo src={LogoWH} />
+                </LogoWrapper>
+                <CopyrightWrapper>
+                    <p>© Copyright 2023 My Voice Up ! | Website by Alexandre Guillin | All Rights Reserved</p>
+                </CopyrightWrapper>
+            </FooterWrapper>
         </FooterContainer>
     );
     
--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -2,8 +2,58 @@ import styled from "styled-components";
 import * as globVars from "../../globalVars";
 import { VoiceUpColors } from "../../colors";
 
+const debug = true;
+
 export const FooterContainer = styled.footer`
     width: 100%;
+    padding: 10px;
     height: ${globVars.sectionHeight}px;
     background-color: ${VoiceUpColors.altGrey};
+    overflow: hidden;
+    ${debug ? "border: 1px solid blue" : ""}
+`;
+
+export const FooterWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    width: 100%;
+    height: 100%;
+    ${debug ? "border: 1px solid green" : ""}
+`;
+
+export const FtNavWrapper = styled.nav`
+    
+    ${debug ? "border: 1px solid orange" : ""}
+`;
+
+export const FtNavList = styled.ul`
+    display: flex;
+    justify-content: center;
+    gap: 150px;
+    list-style-type: none;
+`;
+
+export const FtNavItem = styled.li`
+
+`;
+
+export const SocialWrapper = styled.div`
+    ${debug ? "border: 1px solid purple" : ""}
+`;
+
+export const LogoWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    ${debug ? "border: 1px solid cyan" : ""}
+`;
+
+export const SiteLogo = styled.img`
+    width: 100px;
+`; 
+
+export const CopyrightWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    ${debug ? "border: 1px solid pink" : ""}
 `;
