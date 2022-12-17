129032c57605009c0daf27fc28244e7149de27da
Cap0n3 <cap0n3@protonmail.com>
2022-12-17 18:21:20 +0100

    Finished design of teacher blocks => mod1

--- src/globalVars.js
+++ src/globalVars.js
@@ -2,7 +2,18 @@
  * Global vars for website.
  */
 
+// === ELEMENTS SIZE === //
 export const navbarHeight = 80;
 export const mobileNavbarHeight = 60;
 export const headerHeight = 600;
 export const sectionHeight = 600; // Default section height
+
+// === FONT SIZES === //
+// Medium screen
+export const M_ftSizeXL = "5rem";
+export const M_ftSizeMD = "1.2rem";
+export const M_ftSizeSM = "1rem";
+// Small Screen
+export const S_ftSizeXL = "3rem";
+export const S_ftSizeMD = "1rem";
+export const S_ftSizeSM = "1rem";
