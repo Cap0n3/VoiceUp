34f27dc1db2fdace25e183d7516798f1c6d331b4
Cap0n3 <cap0n3@protonmail.com>
2022-12-19 21:28:58 +0100

    Video implemented, style not finished => mod1

--- src/components/VideoModal/VideoModal.js
+++ src/components/VideoModal/VideoModal.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
-import { VideoThumbnail } from './VideoModal.style';
+import { VideoThumbnail, PlayButton } from './VideoModal.style';
 import VideoWindow from './VideoWindow/VideoWindow';
-
+import { ImPlay } from "react-icons/im";
 
 const VideoModal = ({thumbWidth, vidData}) => {
     const [isOpen, setIsOpen] = useState(false);
     
     return (
         <>
-            <VideoThumbnail onClick={() => setIsOpen(true)} width={thumbWidth} imgSRC={vidData.thumbnail}></VideoThumbnail>   
+            <VideoThumbnail onClick={() => setIsOpen(true)} width={thumbWidth} imgSRC={vidData.thumbnail}>
+                <PlayButton />
+            </VideoThumbnail>   
             <VideoWindow open={isOpen} onClose={() => setIsOpen(false)} vidInfos={vidData} />
             
         </>
--- src/components/VideoModal/VideoModal.style.js
+++ src/components/VideoModal/VideoModal.style.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 import { VoiceUpColors } from "../../colors";
+import { ImPlay } from "react-icons/im";
 
 export const VideoThumbnail = styled.button`
-    display: block;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     background-image: ${({imgSRC}) => `url(${imgSRC})`};
     background-size: 100%;
     background-color: ${VoiceUpColors.black};
@@ -12,4 +15,10 @@ export const VideoThumbnail = styled.button`
     height: ${({width}) => (width ? `${width / 1.77}px` : "19%")};;
     border: 0;
     outline:0;
+`;
+
+export const PlayButton = styled(ImPlay)`
+    fill: white;
+    opacity: 0.7;
+    font-size: 5em;
 `;
