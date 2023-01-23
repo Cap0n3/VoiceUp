14c1d3eaad62dfaf9bba0bd8632298a90aca286f
Cap0n3 <cap0n3@protonmail.com>
2023-01-23 14:23:44 +0100

    Placed contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { ContactSection, FormContainer } from "./Contact.style";
 import ContactForm from '../../components/ContactForm/ContactForm';
 import useWindowSize from '../../hooks/useWindowSize';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import { contactHeaderData } from './data/Contact.data';
 
 const Contact = () => {
     const windowSize = useWindowSize();
 
     return(
-        <ContactSection winHeight={windowSize.innerHeight}>
-            <FormContainer>
-                <ContactForm />
-            </FormContainer>
-        </ContactSection>
+        <>
+            <Header data={contactHeaderData} position={{posX: 0, posY: 50}} />
+            <ContactSection winHeight={windowSize.innerHeight}>
+                <FormContainer>
+                    <ContactForm />
+                </FormContainer>
+            </ContactSection>
+            <Footer />
+        </>
     )
 }
 
---
+++ src/pages/Contact/data/Contact.data.js
@@ -0,0 +1,9 @@
+import ContactHeader from "../../../assets/headers/Header_Contact.jpg";
+
+// HEADER DATA (pic + text)
+export const contactHeaderData = {
+    picURL: ContactHeader,
+    imgTitle: "Contact",
+    titleFR: "Contact",
+    titleEN: "Contact"
+}
