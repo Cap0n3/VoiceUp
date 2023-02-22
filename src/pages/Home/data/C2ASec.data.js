4fec30c3a951f896d5a84844fe6ab5ab2f64ce3f
Cap0n3 <cap0n3@protonmail.com>
2023-02-22 18:31:17 +0100

    Corrected minor problem

--- src/pages/Home/data/C2ASec.data.js
+++ src/pages/Home/data/C2ASec.data.js
@@ -2,9 +2,9 @@
 
 export const C2AData = {
     titleFR: "Progressez",
-    subTitleFR: <><span className="cursiveSpan" style={{textTransform: "uppercase"}}>à votre</span> rythme</>,
+    subTitleFR: <><span className="cursiveSpan">à votre </span>rythme</>,
     titleEN: "Improve",
-    subTitleEN: <><span className="cursiveSpan" style={{textTransform: "uppercase"}}>at your own</span> pace</>,
+    subTitleEN: <><span className="cursiveSpan">at your own </span>pace</>,
     descriptionFR: "Des cours adaptés aux besoins de chacuns, donnés par une professeure de chant diplômée avec plus de 30 ans d’expérience vocale.",
     descriptionEN: "Classes tailored to individual needs, taught by Tania, a certified vocal coach with over 30 years of vocal experience.",
     buttonFR: "Réservez un cours",
